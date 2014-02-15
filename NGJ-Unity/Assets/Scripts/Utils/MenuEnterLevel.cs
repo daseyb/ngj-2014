5d5bb567ea0ce365fa245835a011756edb662553
Dario Seyb <dario.seyb@gmail.com>
2014-02-15 09:10:09 +0100

    -added basic menu scenes

---
+++ NGJ-Unity/Assets/Scripts/Utils/MenuEnterLevel.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MenuEnterLevel : MonoBehaviour {
+	public string LevelToLoad;
+
+	public void Execute() {
+		Application.LoadLevel (LevelToLoad);
+		Time.timeScale = 1;
+	}
+
+	void OnClick() {
+		Execute();
+	}
+}
