b1183c6291e9dbc821d48fa40252fc2a5097f72b
Christian Schubert <schubertchr@gmail.com>
2014-02-15 10:49:57 +0100

    Scripts Initial Checking

---
+++ NGJ-Unity/Assets/Christian/Scripts/ObjectSpawner.cs
@@ -0,0 +1,48 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class ObjectSpawner : MonoBehaviour 
+{
+	public Object diamond;
+	public void Start () 
+	{
+		StartCoroutine ("Spawner");
+	}
+
+	IEnumerator Spawner()
+	{
+        float spawnX = 0;
+        float spawnY = 0;
+		while(true)
+		{
+            switch (Random.Range(0, 5))
+            {
+                case 0: //Left
+                    spawnX = 0;
+                    spawnY = Random.Range(0, Camera.main.pixelHeight);
+                    break;
+                case 1: //Right
+                    spawnX = Camera.main.pixelWidth;
+                    spawnY = Random.Range(0, Camera.main.pixelHeight);
+                    break;
+                case 2: //Top
+                    spawnX = Random.Range(0, Camera.main.pixelWidth);
+                    spawnY = Camera.main.pixelHeight;
+                    break;
+                case 4: //Bottom
+                    spawnX = Random.Range(0, Camera.main.pixelWidth);
+                    spawnY = 0;
+                    break;
+            }
+            
+            Vector3 position = Camera.main.ScreenToWorldPoint(new Vector3(spawnX, spawnY, 0));
+            position.z = 0;
+
+            GameObject obj = (GameObject)Instantiate(diamond, position, Quaternion.identity);
+            GravityScript gs = obj.GetComponent<GravityScript>();
+            gs.center = this.transform;
+
+			yield return new WaitForSeconds(Random.Range(1, 5));
+		}
+	}
+}
---
+++ NGJ-Unity/Assets/PrivacyScript.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PrivacyScript : MonoBehaviour
+{
+	public float radius = 1.0f;
+    public int vertexCount = 20;
+
+    private LineRenderer lineRenderer;
+
+    public void Start()
+    {
+        lineRenderer = gameObject.AddComponent<LineRenderer>();
+    }
+
+    public void OnUpdate()
+    {
+        Color c1 = new Color(0.5f, 0.5f, 0.5f, 1f);
+        lineRenderer.material = new Material(Shader.Find("Particles/Additive"));
+        lineRenderer.SetColors(c1, c1);
+        lineRenderer.SetWidth(0.05f, 0.05f);
+        lineRenderer.SetVertexCount(vertexCount + 1);
+
+        for (var i = 0; i < vertexCount + 1; i++)
+        {
+            float angle = ((float)i / (float)vertexCount) * Mathf.PI * 2;
+
+            float x = radius * Mathf.Cos(angle);
+            float y = radius * Mathf.Sin(angle);
+
+            Vector3 pos = new Vector3(Mathf.Cos(angle), Mathf.Sin(angle), 0) * radius;
+            //Vector3 pos = new Vector3(x, y, 0);
+            lineRenderer.SetPosition(i, pos);
+        }
+    
+    }
+
+
+    public void OnDrawGizmos()
+    {
+        Gizmos.DrawWireSphere(transform.position, radius);
+    }
+
+    public void OnTriggerEnter2D(Collider2D other)
+    {
+        //if(other.tag.Equals("Block"))
+        //{
+            Destroy(other);
+        //}
+    }
+}
